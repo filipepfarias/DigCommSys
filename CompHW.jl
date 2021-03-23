26d4058fc757ca44f3be72d17b3f7c1c382e55cb
Filipe Farias <filipepfarias@fisica.ufc.br>
2021-03-23 11:31:10 -0300

    DigCommSys imported on Binder

--- CompHW.jl
+++ CompHW.jl
@@ -11,8 +11,14 @@ using Plots, DigCommSys
 md"# Digital Communication Systems
 ## Computational Homework (TI0069)
 
-This notebook presents the implementations of the package DigCommSys, developed for the Digital Communication Systems course at UFC. The codes are in [author's GitHub](https://github.com/filippfarias/DigCommSys).
+This notebook presents the implementations of the package DigCommSys, developed for the Digital Communication Systems course at UFC. The codes are in [author's GitHub](https://github.com/filippfarias/DigCommSys) and imported below.
 "
+
+# ╔═╡ 25bc3a22-8be4-11eb-1414-0b8e408a7b54
+begin
+	import Pkg;
+	Pkg.add(url="https://github.com/filippfarias/DigCommSys")
+end
 
 # ╔═╡ f1835c24-8bdc-11eb-2e3e-ffaca518a99c
 md"
@@ -26,6 +32,7 @@ md"
 
 # ╔═╡ Cell order:
 # ╟─8df467aa-8bd9-11eb-088e-218f062da936
+# ╠═25bc3a22-8be4-11eb-1414-0b8e408a7b54
 # ╠═9544fbf0-8bd9-11eb-2a08-ff3364fb0c58
 # ╠═f1835c24-8bdc-11eb-2e3e-ffaca518a99c
 # ╠═c5273a9a-8bd9-11eb-134f-f34babe7faa2
