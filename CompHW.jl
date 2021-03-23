53d11a33b0d9e2abfd758a36d786ff9008d7c6cd
Filipe Farias <filipepfarias@fisica.ufc.br>
2021-03-23 10:44:24 -0300

    Pluto.jl notebook started.

---
+++ CompHW.jl
@@ -0,0 +1,31 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9544fbf0-8bd9-11eb-2a08-ff3364fb0c58
+using Plots, DigCommSys
+
+# ╔═╡ 8df467aa-8bd9-11eb-088e-218f062da936
+md"# Digital Communication Systems
+## Computational Homework (TI0069)
+
+This notebook presents the implementations of the package DigCommSys, developed for the Digital Communication Systems course at UFC. The codes are in [author's GitHub](https://github.com/filippfarias/DigCommSys).
+"
+
+# ╔═╡ f1835c24-8bdc-11eb-2e3e-ffaca518a99c
+md"
+#### Question 1
+
+	1. 
+"
+
+# ╔═╡ c5273a9a-8bd9-11eb-134f-f34babe7faa2
+
+
+# ╔═╡ Cell order:
+# ╟─8df467aa-8bd9-11eb-088e-218f062da936
+# ╠═9544fbf0-8bd9-11eb-2a08-ff3364fb0c58
+# ╠═f1835c24-8bdc-11eb-2e3e-ffaca518a99c
+# ╠═c5273a9a-8bd9-11eb-134f-f34babe7faa2
